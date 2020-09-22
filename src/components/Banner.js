222594efb2c9c1064e749da2956279c6f24f8b4d
Elizabeta Rangjelovikj <elizabetagorgioska@gmail.com>
2020-09-22 14:33:53 +0200

    Modal na banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Modal from './Modal'
 
-export default function Banner (props) {
+export default class Banner extends Component {
+    state = { show: false };
+     
+     showModal = () => {
+       this.setState({ show: true });
+     };
+   
+     hideModal = () => {
+       this.setState({ show: false });
+     };
+   
+   
+     render() {
     
     return (
         <div className="banner">
             <h2>Приклучи се на 1350 ентузијасти и учи дигитални вештини.<br/> Бесплатно.<br/></h2>
             <div className="form">
                 <input type="email" id="email" value="Email Address"></input>
-                <button type="button" onClick={props.showModal}>Пријави се</button>
+                <button type="button" onClick={this.showModal}>Пријави се</button>
             </div>
             <p>Можеш да се исклучиш од листата на мејлови во секое време!</p>
+            <Modal show={this.state.show} handleClose={this.hideModal}/>
         </div>
     )
 }
-
+}
